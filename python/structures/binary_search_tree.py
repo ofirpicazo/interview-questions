9059e8959d775912acdb86587524170899ab0a01
Ofir <ofirpicazo@gmail.com>
2012-03-14 22:31:32 -0700

    Adds find/in-operator methods to the binary tree.

--- python/structures/binary_search_tree.py
+++ python/structures/binary_search_tree.py
@@ -14,6 +14,31 @@ class BinarySearchTree(object):
     def __init__(self):
         self.root = None
 
+    def __getitem__(self, key):
+        """Allows the x[key] syntax for retrieving elements."""
+        if self.root:
+            return self._find(key, self.root)
+        else:
+            raise KeyError(key)
+
+    def __contains__(self, key):
+        """Allows the use of the "in" operator to check for existing keys."""
+        try:
+            value = self.__getitem__(key)
+        except KeyError:
+            return False
+        return True
+
+    def _find(self, key, node):
+        if key == node.key:
+            return node.value
+        elif key < node.key and node.left:
+            return self._find(key, node.left)
+        elif key > node.key and node.right:
+            return self._find(key, node.right)
+        else:
+            raise KeyError(key)
+
     def pre_order(self):
         if self.root:
             return self._pre_order(self.root)
@@ -21,7 +46,7 @@ class BinarySearchTree(object):
             return []
 
     def _pre_order(self, node):
-        sequence = [node.value]
+        sequence = [node.key]
         if node.left:
             sequence.extend(self._pre_order(node.left))
         if node.right:
@@ -38,7 +63,7 @@ class BinarySearchTree(object):
         sequence = []
         if node.left:
             sequence.extend(self._in_order(node.left))
-        sequence.append(node.value)
+        sequence.append(node.key)
         if node.right:
             sequence.extend(self._in_order(node.right))
         return sequence
@@ -55,12 +80,12 @@ class BinarySearchTree(object):
             sequence.extend(self._post_order(node.left))
         if node.right:
             sequence.extend(self._post_order(node.right))
-        sequence.append(node.value)
+        sequence.append(node.key)
         return sequence
 
-    def insert(self, value):
+    def insert(self, key, value):
         """Inserts a new value keeping the tree sorted."""
-        new_node = self._Node(value)
+        new_node = self._Node(key, value)
         if self.root:
             self._insert_child(self.root, new_node)            
         else:
@@ -68,19 +93,20 @@ class BinarySearchTree(object):
 
     def _insert_child(self, parent, child):
         """Insert a child node under a parent node recursively."""
-        if child.value < parent.value:
+        if child.key < parent.key:
             if parent.left:
                 self._insert_child(parent.left, child)
             else:
                 parent.left = child
-        elif child.value > parent.value:
+        elif child.key > parent.key:
             if parent.right:
                 self._insert_child(parent.right, child)
             else:
                 parent.right = child
 
     class _Node(object):
-        def __init__(self, value):
+        def __init__(self, key, value):
+            self.key = key
             self.value = value
             self.left = None
             self.right = None
@@ -89,9 +115,9 @@ class BinarySearchTree(object):
 class BinarySearchTreeTests(unittest.TestCase):
     def setUp(self):
         self.tree = BinarySearchTree()
-        input_values = [10, 6, 7, 8, 9, 5, 11, 20, 16]
-        for value in input_values:
-            self.tree.insert(value)
+        input_keys = [10, 6, 7, 8, 9, 5, 11, 20, 16]
+        for key in input_keys:
+            self.tree.insert(key, key**2)
 
     def test_pre_order(self):
         self.assertEqual(self.tree.pre_order(), [10, 6, 5, 7, 8, 9, 11, 20, 16])
@@ -102,6 +128,15 @@ class BinarySearchTreeTests(unittest.TestCase):
     def test_post_order(self):
         self.assertEqual(self.tree.post_order(), [5, 9, 8, 7, 6, 16, 20, 11, 10])
 
+    def test_get_existing_item(self):
+        self.assertEqual(self.tree[7], 49)
+
+    def test_get_nonexisting_item(self):
+        self.assertRaises(KeyError, self.tree.__getitem__, 50)
+
+    def test_in_operator(self):
+        self.assertTrue(20 in self.tree)
+        self.assertTrue(50 not in self.tree)
 
 if __name__ == '__main__':
     unittest.main()
